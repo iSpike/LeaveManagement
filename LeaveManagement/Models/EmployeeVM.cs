21a2b3593d2f5521c106247f85ee6753459049aa
Pavel <pavel.ivdenko@gmail.com>
2020-04-24 12:18:06 +0300

    Add project files.

---
+++ LeaveManagement/Models/EmployeeVM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaveManagement.Models
+{
+    public class EmployeeVM
+    {
+        public string Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string TaxId { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public DateTime DateJoined { get; set; }
+    }
+}
